cf0453ed921147fbe995915a82c9d320963d8683
Quynhdz19 <Quynhdz19>
2022-08-23 07:49:31 +0700

    done

---
+++ modules/home/lists.php
@@ -0,0 +1,11 @@
+<?php
+if (!defined('_INCODE')) die('Access Deined...');
+$data = [
+    'pageTitle' => 'Tổng quan'
+];
+
+layout('header', 'client', $data);
+?>
+<h1>TRANG CHỦ</h1>
+<?php
+//a();
---
+++ templates/client/layouts/header.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <title>Quản lý người dùng</title>
+    <meta charset="utf-8"/>
+    <link type="text/css" rel="stylesheet" href="<?php echo _WEB_HOST_TEMPLATE; ?>/css/bootstrap.min.css"/>
+    <link type="text/css" rel="stylesheet" href="<?php echo _WEB_HOST_TEMPLATE; ?>/css/font-awesome.min.css"/>
+    <link type="text/css" rel="stylesheet" href="<?php echo _WEB_HOST_TEMPLATE; ?>/css/style.css?ver=<?php echo rand(); ?>"/>
+    <?php
+    head();
+    ?>
+</head>
+<body>
+<header>
+
+</header>
